6160188976e1eee48b92b1bdb11eac19f0b79844
a-fujimya <camu042+github@gmail.com>
2017-01-20 22:30:02 +0900

    using butter knife

--- app/src/main/java/com/example/atyk/hello/MainActivity.java
+++ app/src/main/java/com/example/atyk/hello/MainActivity.java
@@ -5,32 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+  @BindView(R.id.text_area) TextView msg;
+  @BindView(R.id.button_toggle) Button button;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
   }
 
-  @Override protected void onStart() {
-    super.onStart();
-    final TextView msg = (TextView) findViewById(R.id.text_area);
-    final Button button = (Button) findViewById(R.id.button_toggle);
-    button.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        if (msg.getVisibility() == View.INVISIBLE) {
-          button.setText(R.string.button_text_hide);
-          msg.setVisibility(View.VISIBLE);
-        } else {
-          button.setText(R.string.button_text_show);
-          msg.setVisibility(View.INVISIBLE);
-        }
-      }
-    });
-  }
-
-  @Override protected void onStop() {
-    super.onStop();
+  @OnClick(R.id.button_toggle) public void onClickButton() {
+    if (msg.getVisibility() == View.INVISIBLE) {
+      button.setText(R.string.button_text_hide);
+      msg.setVisibility(View.VISIBLE);
+    } else {
+      button.setText(R.string.button_text_show);
+      msg.setVisibility(View.INVISIBLE);
+    }
   }
 }
